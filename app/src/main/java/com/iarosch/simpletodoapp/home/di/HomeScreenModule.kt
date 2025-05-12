4ea99f73a356f451d60c987fd02a6d32f3c0f2d2
iarosch <vit.iarosch@yandex.by>
2025-05-12 11:32:22 +0300

    feat: add DI for usecases from Home module

---
+++ app/src/main/java/com/iarosch/simpletodoapp/home/di/HomeScreenModule.kt
@@ -0,0 +1,25 @@
+package com.iarosch.simpletodoapp.home.di
+
+import com.iarosch.simpletodoapp.home.domain.DeleteTodoUseCase
+import com.iarosch.simpletodoapp.home.domain.GetAllTodoUseCase
+import com.iarosch.simpletodoapp.home.domain.impl.DeleteTodoUseCaseImpl
+import com.iarosch.simpletodoapp.home.domain.impl.GetAllTodoUseCaseImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class HomeScreenModule {
+
+    @Binds
+    abstract fun bindDeleteTodoUseCase(
+        deleteTodoUseCaseImpl: DeleteTodoUseCaseImpl
+    ) : DeleteTodoUseCase
+
+    @Binds
+    abstract fun bindGetAllTodosUseCase(
+        getAllNotesUseCaseImpl: GetAllTodoUseCaseImpl
+    ) : GetAllTodoUseCase
+}
