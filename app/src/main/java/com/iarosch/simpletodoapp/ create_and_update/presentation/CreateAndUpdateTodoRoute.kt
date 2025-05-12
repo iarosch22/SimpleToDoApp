fc2ff46c1e6f29c852e539c0653d14ab37bcd005
iarosch <vit.iarosch@yandex.by>
2025-05-12 14:01:11 +0300

    feat: add route for create_and_update module

---
+++ app/src/main/java/com/iarosch/simpletodoapp/ create_and_update/presentation/CreateAndUpdateTodoRoute.kt
@@ -0,0 +1,28 @@
+package com.iarosch.simpletodoapp.create_and_update.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+internal fun CreateAndUpdateNoteRoute(
+    modifier: Modifier = Modifier,
+    viewModel: CreateAndUpdateTodoViewModel = hiltViewModel(),
+    topBarTitle: String?,
+    navigateToHomeScreen: () -> Unit
+) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    CreateAndUpdateNoteScreen(
+        modifier = modifier,
+        uiState = uiState,
+        topBarTitle = topBarTitle,
+        onSaveButtonClick = { viewModel.handleEvent(CreateAndUpdateTodoUiEvent.OnSaveClicked) },
+        navigateToHomeScreen = navigateToHomeScreen,
+        onTitleChanged = { viewModel.handleEvent(CreateAndUpdateTodoUiEvent.OnTitleChanged(it)) },
+        onDescriptionChanged = { viewModel.handleEvent(CreateAndUpdateTodoUiEvent.OnDescriptionChanged(it)) }
+    )
+}
